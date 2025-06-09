a0a2e76c3220f2ce07999fd6f39ca6fc6a6b78fa
royd2023 <rdinh2023@gmail.com>
2025-06-09 14:52:23 -0400

    added empty home page

--- boxure-frontend/src/app/page.js
+++ boxure-frontend/src/app/page.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import Home from './Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<h1>Welcome to the Home Page!</h1>} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
