f21f7920cdd09a52ce71790f83d91c38e02edbfa
sam-medley <Blazer2sam@gmail.com>
2025-07-02 02:20:05 -0400

    test mock so no db connection is needed

--- boxure-services/test/basic.test.js
+++ boxure-services/test/basic.test.js
@@ -2,9 +2,10 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe.skip('GET /api/items', () => {
-  it('should return 200', async () => {
-    const res = await request(app).get('/api/items');
-    expect(res.statusCode).toBe(200);
-  });
+jest.mock('pg', () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn().mockResolvedValue({ rows: [] }),
+  };
+  return { Client: jest.fn(() => mClient) };
 });
