99d0ab4a74594a7246475865df47442da30166a9
sam-medley <Blazer2sam@gmail.com>
2025-09-08 20:30:00 -0400

    package

--- boxure-frontend/src/app/home/page.js
+++ boxure-frontend/src/app/home/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/na            vigation";
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 
